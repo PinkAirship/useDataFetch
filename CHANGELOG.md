3e3b672898b5fdadf8ba179c8759c1008b91f85d
Marc Phillips <mphillips@pinkairship.com>
2021-03-10 12:24:17 -0700

    Prep for version bump

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,16 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [3.1.1] - 2021-02-13
+
+### Added
+
+- Added esm, cjs exports
+
+### Changed
+
+### Removed
+
 ## [3.1.0] - 2021-02-13
 
 ### Added
--- babel.config.js
+++ babel.config.js
@@ -2,14 +2,15 @@ module.exports = (api) => {
   // api.cache(true)
   return {
     presets: [
-      api.env('development') && '@babel/preset-env',
-      api.env('development') && '@babel/preset-react',
+      api.env(['development', 'test']) && '@babel/preset-env',
+      api.env(['development', 'test']) && '@babel/preset-react',
     ].filter(Boolean),
     plugins: [
       'transform-react-jsx',
-      api.env('development') &&
+      api.env(['development', 'test']) &&
         '@babel/plugin-proposal-object-rest-spread',
-      api.env('development') && '@babel/plugin-transform-runtime',
+      api.env(['development', 'test']) &&
+        '@babel/plugin-transform-runtime',
     ].filter(Boolean),
   }
 }
